1b4edbb84283269db2f53a4f88dec579c2379b89
victorolatunji <victor.olatunji@dcmail.ca>
2025-07-21 17:38:06 -0400

    Fourth Commit
    Declared string variables and arrays

---
+++ lab4/part1/main.js
@@ -0,0 +1,27 @@
+// Name: Victor Olatunji
+// File: lab4
+// Date: 21 July 2025
+// Lab 4 Part 1 Silly story generator
+// Course: Web Development Fundamentals (INFT1206)
+
+
+// 1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
+
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+// 2. RAW TEXT STRINGS
+
+let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+
+let insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+
+let insertY = ["the soup kitchen", "Disneyland", "the White House"];
+
+let insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
