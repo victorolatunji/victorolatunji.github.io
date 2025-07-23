c961341904625ae75f6fe9f2a75f28c4f33689c4
victorolatunji <victor.olatunji@dcmail.ca>
2025-07-23 15:16:50 -0400

    Third commit
    Declared array of file names and alts

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -11,14 +11,18 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const alts = {
+            'pic1.jpg': 'Closeup of Human eye', 
+            'pic2.jpg': 'Wavy Rock ', 
+            'pic3.jpg': 'purple & white flowers',
+            'pic4.jpg': 'Egyptian Drawing',
+            'pic5.jpg': 'Butterfly'
+            }
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
