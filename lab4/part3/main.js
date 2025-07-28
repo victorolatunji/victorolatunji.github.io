ab417aa727fa02b9101f06966cf6e169949ff8f2
victorolatunji <victor.olatunji@dcmail.ca>
2025-07-28 18:29:54 -0400

    Second Commit
    Added appropriate comment headers

--- lab4/part3/main.js
+++ lab4/part3/main.js
@@ -1,3 +1,9 @@
+// Name: Victor Olatunji
+// File: lab4 
+// Date: 28 July 2025
+// Lab 4 Part 3 Object Building Practice
+// Course: Web Development Fundamentals (INFT1206)
+
 // setup canvas
 
 const canvas = document.querySelector("canvas");
