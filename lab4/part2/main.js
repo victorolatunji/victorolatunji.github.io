d671e382d8609aee673c77f7ae59ccb1f105e0cc
victorolatunji <victor.olatunji@dcmail.ca>
2025-07-30 17:19:10 -0400

    Final Commit
    Added all the provided codes in the JavaScript file.

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -12,7 +12,7 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
-
+     
 /* Declaring the alternative text for each image file */
 const alts = {
             'pic1.jpg': 'Closeup of Human eye', 
